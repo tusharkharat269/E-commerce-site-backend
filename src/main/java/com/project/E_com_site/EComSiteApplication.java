9d2ad8fe10975533676f57945451df37e8731424
tusharkharat269 <tusharkharat003@gmail.com>
2025-02-18 11:10:04 +0530

    Intitialize the project with dependecies

---
+++ src/main/java/com/project/E_com_site/EComSiteApplication.java
@@ -0,0 +1,13 @@
+package com.project.E_com_site;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EComSiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EComSiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/E_com_site/EComSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.E_com_site;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EComSiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
